67b218ae1be260d29cb89a489c1d37ba686b960d
aminul-islam01 <abiddut953@gmail.com>
2023-04-20 01:50:50 +0600

    dragon news server site

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const categories = require('./data/categories.json')
+
+
+
+app.get('/', (req, res) => {
+  res.send('hello world ok sdfsaf dsfa')
+})
+
+app.get('/categories', (req, res) => {
+  res.send(categories)
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`dragon api is running on port: ${port}`)
+})
